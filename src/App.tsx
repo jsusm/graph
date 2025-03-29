00a56d8e36a465d45371dd1f0fc26209a5fb11e8
jsusm <jesusjaviermarcanomora.2005@gmail.com>
2025-03-29 16:53:46 -0400

    Drag and drop

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,85 @@
 import { useEffect, useReducer, useRef, useState } from "react"
 
-type ActionState = 'idle' | 'draging'
+type ActionState = 'idle' | 'selecting' | 'dragging'
 
-function actionReducer(state: ActionState, action: string) {
-  // Node transitions
-  if (state == 'idle') {
-    switch (action) {
-      case 'mousedown':
-        return 'draging'
+const colors = {
+  red: "oklch(0.645 0.246 16.439)",
+  blue: "oklch(0.707 0.165 254.624)",
+}
+
+type DrawNode = {
+  bounding: [number, number, number, number];
+  type: 'square' | 'circle';
+  color: keyof typeof colors;
+  path: Path2D;
+}
+
+type DrawState = {
+  actionState: ActionState;
+  selectedNode: number;
+  objects: DrawNode[];
+  mouse: {
+    relativeOffset: [number, number]
+  }
+}
+
+function draw(c: CanvasRenderingContext2D, state: DrawState) {
+  // Clear Canvas
+  c.beginPath()
+  c.clearRect(0, 0, c.canvas.width, c.canvas.height)
+
+  // Draw objects
+  for (let i = 0; i < state.objects.length; i++) {
+    const obj = state.objects[i]
+    // check if object is in screen
+
+
+    if (obj.type == 'square') {
+      c.save()
+      c.beginPath()
+      obj.path = new Path2D()
+      obj.path.rect(obj.bounding[0], obj.bounding[1], obj.bounding[2], obj.bounding[3])
+      c.strokeStyle = colors[obj.color]
+      c.stroke(obj.path)
+      c.restore()
     }
   }
-  if (state == 'draging') {
-    switch (action) {
-      case 'mouseup':
-        return 'idle';
-    }
-  }
-  throw Error(`Unknown action: ${action} for state ${state}`)
 }
 
 function App() {
   const canvas = useRef<HTMLCanvasElement>(null)
   const ctx = useRef<CanvasRenderingContext2D | null>(null)
-  const [mousePos, setMousePos] = useState({ x: 0, y: 0, lx: 0, ly: 0 })
-  const [actionState, dispatchAction] = useReducer(actionReducer, 'idle')
-  const action = useRef('idle')
 
-  const [shapeOrigin, setShapeOriging] = useState({ x: 0, y: 0 })
+  const [drawState, setDrawState] = useState<DrawState>({
+    actionState: 'idle', objects: [
+      {
+        type: 'square',
+        bounding: [40, 40, 80, 80],
+        color: 'red',
+        path: new Path2D(),
+      },
+      {
+        type: 'square',
+        bounding: [40, 40, 80, 80],
+        color: 'blue',
+        path: new Path2D(),
+      }
+    ], selectedNode: -1,
+    mouse: {
+      relativeOffset: [0, 0]
+    },
+  })
+
+  const refDrawState = useRef<DrawState>(drawState)
 
   useEffect(() => {
-    action.current = actionState
-  }, [actionState])
+    refDrawState.current = drawState
+  }, [drawState])
 
   useEffect(() => {
     const rect = window.document.body.getBoundingClientRect()
 
+    // set up canvas context
     const c = canvas.current?.getContext('2d')
     if (c && canvas.current) {
       canvas.current.width = rect.width
@@ -46,69 +91,78 @@ function App() {
       ctx.current = c
     }
 
-    const mouseover = (e: MouseEvent) => {
-      setMousePos(s => ({ x: e.clientX, y: e.clientY, lx: s.x, ly: s.y }))
-    }
-    canvas.current?.addEventListener('mousemove', mouseover)
+    setupListeners()
 
-    addEventListener('pointerrawupdate', (e) => {
-      console.log(e)
+    // Resize canvas when window is resized
+    window.addEventListener('resize', _ => {
+      if (canvas.current) {
+        canvas.current.width = window.innerWidth
+        canvas.current.height = window.innerHeight
+      }
     })
 
-    const mousedown = (e: MouseEvent) => {
-      if (action.current == 'idle') {
-        dispatchAction('mousedown')
-        setShapeOriging({ x: e.clientX, y: e.clientY })
+    function loop() {
+      if (c) {
+        draw(c, refDrawState.current)
       }
+      requestAnimationFrame(loop)
     }
-    canvas.current?.addEventListener('mousedown', mousedown)
-
-    const mouseup = (_: MouseEvent) => {
-      if (action.current == 'draging') {
-        dispatchAction('mouseup')
-      }
-    }
-    canvas.current?.addEventListener('mouseup', mouseup)
-
-    return () => {
-      canvas.current?.removeEventListener('mousemove', mouseover)
-      canvas.current?.removeEventListener('mouseup', mouseup)
-      canvas.current?.removeEventListener('mousedown', mousedown)
-    }
+    loop()
   }, [])
 
-  useEffect(() => {
-    if (ctx.current != null) {
-      const c = ctx.current
-      c.clearRect(0, 0, c.canvas.width, c.canvas.height)
+  const setupListeners = () => {
+    window.addEventListener('mousemove', (e) => {
+      const ds = refDrawState.current
 
-      c.beginPath()
-      c.translate(mousePos.x, mousePos.y)
-      c.rect(-10, -10, 20, 20)
-      c.stroke()
-      c.resetTransform()
-
-
-      if (actionState == "draging") {
-        c.save()
-        c.beginPath()
-        c.translate(shapeOrigin.x, shapeOrigin.y)
-        c.rect(0, 0, mousePos.x - shapeOrigin.x, mousePos.y - shapeOrigin.y)
-        c.stroke()
-        c.restore()
+      if (ds.actionState == 'dragging') {
+        const objects = ds.objects
+        const objBounding = objects[ds.selectedNode].bounding
+        objects[ds.selectedNode].bounding = [
+          ds.mouse.relativeOffset[0] + e.clientX,
+          ds.mouse.relativeOffset[1] + e.clientY,
+          objBounding[2],
+          objBounding[3]
+        ]
+        setDrawState(s => ({ ...s, objects }))
       }
+    })
 
-    }
-  }, [mousePos])
+    canvas.current?.addEventListener('pointerdown', e => {
+      setDrawState(ds => ({ ...ds, mouse: { ...ds.mouse, pressedMousePosition: [e.clientX, e.clientY] } }))
+
+      const ds = refDrawState.current
+      for (let i = ds.objects.length - 1; i >= 0; i--) {
+        const obj = ds.objects[i];
+        if (ctx.current?.isPointInStroke(obj.path, e.clientX, e.clientY)) {
+          setDrawState(s => ({
+            ...s,
+            selectedNode: i,
+            actionState: 'dragging',
+            mouse: {
+              relativeOffset: [obj.bounding[0] - e.clientX, obj.bounding[1] - e.clientY]
+            }
+          }))
+          break
+        }
+      }
+    })
+    canvas.current?.addEventListener('pointerup', e => {
+      const ds = refDrawState.current
+      if (ds.actionState == 'dragging') {
+        setDrawState(s => ({ ...s, actionState: 'selecting' }))
+      }
+    })
+  }
 
   return (
     <main className="bg-stone-900 min-h-screen text-white">
       <canvas ref={canvas}>
       </canvas>
 
-      <div className="absolute top-0 left-0">
+      <div className="absolute top-0 left-0 pointer-events-none">
         <pre>
-          state: {actionState}
+          action: {drawState.actionState} <br />
+          nodeSelected: {drawState.selectedNode}<br />
         </pre>
       </div>
     </main>
